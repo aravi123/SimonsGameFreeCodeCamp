c1955c75487b622882fe31a323d814aea518761a
aravi123 <aravindcranjan54@gmail.com>
2016-12-25 22:54:25 +0530

    Final Game as per Freecodecamp

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 $(document).ready(function () {
-	var id = 0,i=0,k=0,x,d=0;
+	var id = 0,i=0,k=0,x,d=0,lives=3;
 	var order = [];
 	$('.score').append(0);
 	$('.lives').append(3);
 	$('.start').click(function () {
 			console.log("called");
+			lives=3;
+			id=0;
+			$('.score').html("Score:0");
+			$('.lives').html("Lives Remaining:3");
+			d=0;
 			statgame();
+			$('.start').hide();
+			$('.game').show();
 	});
+	$('.strict').click(function () {
+			console.log("called");
+			lives=0;
+			id=0;
+			$('.score').html("Score:0");
+			$('.lives').html("");
+			d=0;
+			statgame();
+			$('.start').hide();
+			$('.strict').hide();
+			$('.game').show();
+	});
+	function PlaySound(y) {
+				if (y==1) {
+					var sound = document.getElementById("audio1");
+				}
+				else if (y==2) {
+					var sound = document.getElementById("audio2");
+				}
+				else if (y==3) {
+					var sound = document.getElementById("audio3");
+				}
+				else {
+					var sound = document.getElementById("audio4");
+				}
+				 sound.play()
+		 }
 	function statgame() {
 			if (d<=id) {
 				order[id] = Math.ceil(Math.random()*4);
 					setTimeout(function(){
 					$('#'+order[d]).css({
 						'background-color':'red'
-					});},200);
+					});
+					PlaySound(order[d]);
+				},200);
 					setTimeout(function(){
 						$('#'+order[d++]).css({
 							'background-color':'whitesmoke'
@@ -24,10 +60,22 @@ $(document).ready(function () {
 			else {
 				console.log(order);
 			}
-
+	}
+	function showagain() {
+		if (d<=id) {
+			setTimeout(function(){
+				$('#'+order[d]).css({'background-color':'red'});
+				PlaySound(order[d]);
+			},200);
+			setTimeout(function(){
+				$('#'+order[d++]).css({'background-color':'whitesmoke'});
+				showagain();
+			},500);
+		}
 	}
 		$('.gg').click(function(){
 			x = $(this).attr('id');
+			PlaySound(x);
 			//console.log(x);
 			$('#'+x).css({
 				'background-color':'green'
@@ -41,7 +89,17 @@ $(document).ready(function () {
 			x= null;
 		}
 		else if(x!=order[k]){
-			console.log("game over");
+			if (lives!=0) {
+				lives--;
+				$('.lives').html("Lives Remaining:"+lives);
+				d=0;
+				showagain();
+			}
+			else{
+				$('.game').hide();
+				$('.start').show();
+				$('.strict').show();
+			}
 		}
 		if (k==id+1) {
 			k=0;
